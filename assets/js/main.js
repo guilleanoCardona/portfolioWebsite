d1b5a7fbc5028824b8c9f081a5ccc66cc9477654
Guilleano Cardona <gcardona@tecnoglass.com>
2023-10-18 17:20:04 -0500

    80%

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,16 @@ function changeToogleIcon() {
 function addActiveLink() {
   const section = document.querySelectorAll("section[id]");
   section.forEach((section) => {
-    const scrollY = window.screenY,
+    const scrollY = window.scrollY,
       sectionTop = section.offsetTop - 80,
       sectionHeight = section.offsetHeight,
       sectionId = section.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__link[href*=" + sectionId + "]")
+        .classList.add("nav__link--active");
+    } else {
       document
         .querySelector(".nav__link[href*=" + sectionId + "]")
         .classList.remove("nav__link--active");
@@ -41,3 +45,42 @@ function addActiveLink() {
 }
 
 window.addEventListener("scroll", () => {});
+
+// increment counter
+function startCounter(counter) {
+  // Get the target number
+  const targetNumber = counter.getAttribute("data-target");
+  const increment = setInterval(() => {
+    counter.textContent++;
+    if (counter.textContent == targetNumber) {
+      clearInterval(increment);
+    }
+  }, 2000 / targetNumber);
+}
+
+const counterSection = document.querySelector(".counter");
+const counters = document.querySelectorAll(".counter__number");
+let started = false;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= counterSection.offsetTop - 400) {
+    if (!started) {
+      counters.forEach((counter) => startCounter(counter));
+    }
+    started = true;
+  }
+});
+
+// Testimonial Swiper
+
+const TestimonialSwiper = new Swiper(".testimonial__wrapper", {
+  spaceBetween: 40,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
